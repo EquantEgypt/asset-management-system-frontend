fbfdbbd068c2134d6fa78adce10bb4029c7cec21
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-09-03 22:47:10 +0300

    added mock dashboards , Services & routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { EmployeeDashboard } from './components/employee-dashboard/employee-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'dashboard',   // fixed typo: "dasboard" → "dashboard"
+    component: EmployeeDashboard
+  }
+];
