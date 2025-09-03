6322efb6d5a852783406b24cda7d9799bd2b3268
Maryiamreda <maryiamreda@gmail.com>
2025-09-03 22:15:23 +0200

    added Routes Role Guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,6 @@
-import { ActivatedRouteSnapshot, CanActivate, Router } from "@angular/router";
-import { AuthService } from "../services/auth.service";
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +8,11 @@ import { Injectable } from "@angular/core";
 export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
-    const requiresAuth = route.data['requiresAuth'] === true;
-
-    if (requiresAuth && !this.auth.isAuthenticated()) {
-      this.router.navigate(['/users/login']);
-      return false;
+  canActivate(): boolean {
+    if (this.auth.isAuthenticated()) {
+      return true; // allow access
     }
-
-    if (!requiresAuth && this.auth.isAuthenticated()) {
-      this.router.navigate(['/users/me/tasks']);
-      return false;
-    }
-
-    return true;
+    this.router.navigate(['/login']);
+    return false; // block access
   }
 }
