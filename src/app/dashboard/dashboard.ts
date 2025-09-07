e999a526300091d29f0cf856066f501106cfff48
Maryiamreda <maryiamreda@gmail.com>
2025-09-07 11:40:30 +0200

    Install Angular Material

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { UsersContainer } from '../../components/users-container/users-container';
+import { AssetsContainer } from '../../components/assets-container/assets-container';
+import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css'
+  selector: 'app-admin-dashboard',
+  imports: [UsersContainer,AssetsContainer,CommonModule , MatTableModule],
+  templateUrl: './admin-dashboard.html',
+  styleUrl: './admin-dashboard.css'
 })
 export class Dashboard {
   constructor(private auth: AuthService, private router: Router) { }
