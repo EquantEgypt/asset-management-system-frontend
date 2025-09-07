21b9199d846e389e42d0b5fcdf5bbde6f2bc7446
Maryiamreda <maryiamreda@gmail.com>
2025-09-07 15:12:35 +0200

    Address PR review comments

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.auth.isAuthenticated()) {
-      return true; // allow access
+  canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
+    
+    if (state.url === '/login') {
+      if (this.auth.isAuthenticated()) {
+        this.router.navigate(['/dashboard']);
+        return false;  // block access
+      }
+      return true;
     }
-    this.router.navigate(['/login']);
-    return false; // block access
+
+    if (!this.auth.isAuthenticated()) {
+      this.router.navigate(['/login']);
+      return false;  // block access
+    }
+
+    return true;
   }
 }
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -3,29 +3,35 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { faEye, faEyeSlash, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { ToastService } from 'angular-toastify';
 
 @Component({
-    selector: 'app-login',
-    imports: [ReactiveFormsModule, CommonModule],
-    templateUrl: './login.html',
-    styleUrl: './login.css'
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
 })
 
 export class Login {
-
+    faEye = faEye;
+    faEyeSlash = faEyeSlash;
+    faEnvelope = faEnvelope;
+    faLock = faLock;
     showPassword = false;
-    errorMessage: string | null = null;
-    constructor(private authService: AuthService, private router: Router, private toast: ToastService) { }
+
+    constructor(private authService: AuthService, private router: Router, private toast: ToastService) {}
 
     profileForm = new FormGroup({
         email: new FormControl('', [
-            Validators.required,
-            Validators.email,
+            Validators.required, 
+            Validators.email,   
             Validators.pattern(/^[a-zA-Z0-9._%+-]+@orange.com$/)
         ]),
         password: new FormControl('', [
-            Validators.required
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).+$/)
         ]),
         keepLoggedIn: new FormControl(false)
     });
@@ -33,37 +39,44 @@ export class Login {
     get email() {
         return this.profileForm.get('email');
     }
-
+    
     get password() {
         return this.profileForm.get('password');
     }
-
+    
     get keepLoggedIn() {
         return this.profileForm.get('keepLoggedIn');
     }
-
+    
     togglePassword() {
         this.showPassword = !this.showPassword;
     }
 
 
-    onSubmit() {
+ onSubmit() {
         const email = this.email?.value!;
         const password = this.password?.value!;
 
+        if (this.profileForm.invalid  ) {
+            this.profileForm.markAllAsTouched();
+                  this.toast.error('Please fill in all required fields correctly.');
 
-        const keepLoggedIn = this.keepLoggedIn?.value || false;
-        if (this.profileForm.valid) {
-            this.authService.login(email, password, keepLoggedIn).subscribe({
-                next: (user) => {
-                    this.errorMessage = null
-
-                },
-                error: (err) => {
-                    this.errorMessage = 'Invalid email or password. Please try again.';
-                }
-            });
+            return;
         }
 
+        
+        const keepLoggedIn = this.keepLoggedIn?.value || false; 
+
+        this.authService.login(email, password, keepLoggedIn).subscribe({
+            next: (user) => {
+                this.toast.success('Login successful')
+            },
+            error: (err) => {
+                let message='Login Failed';
+                if(err.status==401) message='wrong credentials';
+                 else if(err.status==500) message='server error ';
+                this.toast.error(message);
+            }
+        });
     }
 }
