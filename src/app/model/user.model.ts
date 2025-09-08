37088ccd4fadd9f398b10bcf57a6e9104dd82285
Maryiamreda <maryiamreda@gmail.com>
2025-09-08 12:33:18 +0200

    logic of the submit  and remove toast

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -3,9 +3,8 @@ export interface User {
   id: number;
   username: string;
   email: string;
-  role: Role;          // matches backend "role"
+  role: Role;       
   departmentName: string;
-  // password?: string; // only include if you need it on frontend forms
 }
 export interface Role {
   roleId: number;
