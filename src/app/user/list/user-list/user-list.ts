ef45fc659c652ac27965a2a201c8f951685a1834
Maryiamreda <maryiamreda@gmail.com>
2025-09-08 08:40:15 +0200

    complete rebase

--- src/app/user/list/user-list/user-list.ts
+++ src/app/user/list/user-list/user-list.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { User } from '../../../model/user.model';
+import {users} from '../../../services/assets.service'
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css'
 })
 export class UserList {
+users: User[] = users;
 
+  displayedColumns: string[] = ['id', 'username', 'email', 'role', 'department'];
 }
