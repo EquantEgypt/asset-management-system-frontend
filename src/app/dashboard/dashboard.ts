2ac7648b35101dbee49bc0cb80f936cb1bbfd1f1
Maryiamreda <maryiamreda@gmail.com>
2025-09-11 19:49:48 +0200

    Sharing modules

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -13,10 +13,22 @@ import { Role } from '../model/roles.enum';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard  implements OnInit {
+  role: Role | null = null;
+  activeTab: any= 'assets'; 
+
   constructor(private auth: AuthService, private router: Router) { }
+
+setActive(tab: 'assets' | 'users') {
+    this.activeTab = tab;
+}
+ngOnInit() {
+    this.role = this.auth.getRole();
+  }
+
   logout() {
     this.auth.logout();
+
     this.router.navigate(['/login']);
   }
 }
