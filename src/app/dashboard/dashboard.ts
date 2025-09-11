46b3bb5dcab5ffd2355e8d8e8173ccbd7d5741da
Maryiamreda <maryiamreda@gmail.com>
2025-09-11 12:24:12 +0200

    only admin or manager can access users list component

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -2,36 +2,34 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { UserList } from '../user/list/user-list/user-list';
 import { AssetList } from '../asset/list/asset-list/asset-list';
 import { Role } from '../model/roles.enum';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [UserList,AssetList,CommonModule , MatTableModule],
+  imports: [UserList, AssetList, CommonModule, MatTableModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class Dashboard {
-    role: Role | null =null;
-constructor(private auth:AuthService, private router: Router){}
+  role: Role | null = null;
+  constructor(private auth: AuthService, private router: Router) { }
 
- ngOnInit() { // Fixed: was "ngOnItit"
-  console.log("this is te dashboard and the role is " , this.auth.getRole)
-    this.role = this.auth.getRole(); 
+  ngOnInit() {
+    console.log("this is te dashboard and the role is ", this.auth.getRole)
+    this.role = this.auth.getRole();
   }
-  //only admin or manager can access the top header 
-  canAccessUsers(): boolean {
-  return this.role === 'Admin' || this.role === 'Department_Manager';
-}
 
 
-activeTab: any= 'assets'; 
-setActive(tab: 'assets' | 'users') {
+
+
+  activeTab: any = 'assets';
+  setActive(tab: 'assets' | 'users') {
     this.activeTab = tab;
-}
- logout() {
+  }
+  logout() {
     this.auth.logout();
     this.router.navigate(['/login']);
   }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,30 +3,29 @@ import { AuthService } from "./auth.service";
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { User } from "../model/user.model";
 
-  const BACKEND_URL = 'http://localhost:8080/get/users';
+const BACKEND_URL = 'http://localhost:8080/get/users';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService{
+export class UserService {
 
-  constructor(private http: HttpClient,private auth: AuthService, private router: Router) {}
+  constructor(private http: HttpClient, private auth: AuthService, private router: Router) { }
 
 
-getUsers(page: number, size: number,username?:string ,departmentId?:  number | ''): Observable<any>{
-const token= this.auth.getAuthToken();
-let url = `${BACKEND_URL }?page=${page}&size=${size}`;
+  getUsers(page: number, size: number, username?: string, departmentId?: number | ''): Observable<any> {
+    const token = this.auth.getAuthToken();
+    let url = `${BACKEND_URL}?page=${page}&size=${size}`;
     if (username && username.trim() !== '') {
       url += `&username=${encodeURIComponent(username)}`;
     }
-     if (departmentId !== '' && departmentId !== null && departmentId !== undefined) {
+    if (departmentId !== '' && departmentId !== null && departmentId !== undefined) {
       url += `&departmentId=${encodeURIComponent(departmentId)}`;
     }
-    const header= new HttpHeaders().set("Authorization",`Bearer ${token}`)
-    return this.http.get<any>(url,{
-    headers: { Authorization: `Basic ${token}` }
-  });
-}
+    const header = new HttpHeaders().set("Authorization", `Bearer ${token}`)
+    return this.http.get<any>(url, {
+      headers: { Authorization: `Basic ${token}` }
+    });
+  }
 }
