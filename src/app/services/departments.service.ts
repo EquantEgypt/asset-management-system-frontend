f144f4e15e293ef72e4c5ef474485a115061a426
Maryiamreda <maryiamreda@gmail.com>
2025-09-13 10:25:54 +0200

    change endpoint

--- src/app/services/departments.service.ts
+++ src/app/services/departments.service.ts
@@ -1,24 +1,19 @@
-import { Router } from "@angular/router";
 import { AuthService } from "./auth.service";
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { User } from "../model/user.model";
 import { Department } from "../model/department.model";
 
-const BACKEND_URL = 'http://localhost:8080/get';
+const BACKEND_URL = 'http://localhost:8080/api/departments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
-
-  constructor(private http: HttpClient, private auth: AuthService, private router: Router) { }
-
-
+  constructor(private http: HttpClient, private auth: AuthService) { }
   getDepartmentsName(): Observable<Department[]> {
     const token = this.auth.getAuthToken();
-    return this.http.get<Department[]>(`${BACKEND_URL}/departments`, {
+    return this.http.get<Department[]>(`${BACKEND_URL}`, {
       headers: { Authorization: `Basic ${token}` }
     });
   }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,12 +1,11 @@
-import { Router } from "@angular/router";
 import { AuthService } from "./auth.service";
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "../model/user.model";
 
-const BACKEND_URL = 'http://localhost:8080/get/users';
- interface Page<T> {
+const BACKEND_URL = 'http://localhost:8080/api/users';
+interface Page<T> {
   content: T[];
   page: {
     size: number;
@@ -21,22 +20,20 @@ const BACKEND_URL = 'http://localhost:8080/get/users';
 })
 export class UserService {
 
-  constructor(private http: HttpClient, private auth: AuthService, private router: Router) { }
-
-
-  getUsers(page: number, size: number, searchWord?: string, departmentId?: number | '',filteredRole?:string|''): Observable<Page<User>> {
+  constructor(private http: HttpClient, private auth: AuthService,) { }
+  getUsers(page: number, size: number, searchWord?: string, departmentId?: number | '', filteredRole?: string | ''): Observable<Page<User>> {
     const token = this.auth.getAuthToken();
 
-    let params=new HttpParams().set('page',page).set('size',size);
+    let params = new HttpParams().set('page', page).set('size', size);
     if (searchWord && searchWord.trim() !== '') {
-     params=params.set('search',searchWord);
+      params = params.set('search', searchWord);
     }
 
     if (filteredRole && filteredRole.trim() !== '') {
-     params=params.set('role',filteredRole);
+      params = params.set('role', filteredRole);
     }
     if (departmentId !== '' && departmentId !== null && departmentId !== undefined) {
-           params=params.set('departmentId',departmentId);
+      params = params.set('departmentId', departmentId);
     }
     return this.http.get<Page<User>>(`${BACKEND_URL}`, {
       headers: { Authorization: `Basic ${token}` },
