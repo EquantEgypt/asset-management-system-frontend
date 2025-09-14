a6fd5b7fb82cb12c602de14d89325a887fb080d7
nourhan.hisham <nourhan.hisham.ext@orange.com>
2025-09-14 14:53:15 +0300

    fix dashboard

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import {MatTableModule} from '@angular/material/table';
 import { UserList } from '../user/list/user-list/user-list';
 import { AssetList } from '../asset/list/asset-list/asset-list';
+import { Role } from '../model/roles.enum';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,9 +14,21 @@ import { AssetList } from '../asset/list/asset-list/asset-list';
   styleUrl: './dashboard.css'
 })
 export class Dashboard {
+  role: Role | null = null;
+  activeTab: any= 'assets'; 
+
   constructor(private auth: AuthService, private router: Router) { }
+
+setActive(tab: 'assets' | 'users') {
+    this.activeTab = tab;
+}
+ngOnInit() {
+    this.role = this.auth.getRole();
+  }
+
   logout() {
     this.auth.logout();
+
     this.router.navigate(['/login']);
   }
 }
