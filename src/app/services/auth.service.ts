2e1ca61b8bf7db69bb940252427cbb07a1efaad7
Zeyad <farghlyzeyad9@gmail.com>
2025-09-20 23:53:03 +0300

    edit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -58,6 +58,15 @@ export class AuthService {
     return user?.id ?? null;
   }
 
+    getCurrentUsername(): string | null {
+    const userStr = this.storage.getItem(USER);
+    if (!userStr) {
+      return null;
+    }
+    const user = JSON.parse(userStr);
+    return user?.username ?? null;
+  }
+
   getAuthToken(): string | null {
     return this.storage.getItem(AUTH_TOKEN);
   }
