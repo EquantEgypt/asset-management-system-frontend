d290ba485bc653b2594607231e103e7579e4b75c
Zeyad <farghlyzeyad9@gmail.com>
2025-09-21 13:17:24 +0300

    edit display

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { UserList } from '../user/list/user-list/user-list';
 import { AssetList } from '../asset/list/asset-list/asset-list';
 import { Role } from '../model/roles.enum';
@@ -10,26 +10,27 @@ import { AddRequestComponent } from '../request/add/add-request.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [UserList,AssetList,CommonModule , MatTableModule, AddRequestComponent],
+  imports: [UserList, AssetList, CommonModule, MatTableModule, AddRequestComponent],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
   role: Role | null = null;
-  activeTab: any= 'assets'; 
+  Role = Role; 
+  activeTab: 'assets' | 'users' | 'requests' = 'assets';
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) {}
 
-setActive(tab: 'assets' | 'users' | 'requests') {
-    this.activeTab = tab;
-}
-ngOnInit() {
+  ngOnInit() {
     this.role = this.auth.getRole();
+  }
+
+  setActive(tab: 'assets' | 'users' | 'requests') {
+    this.activeTab = tab;
   }
 
   logout() {
     this.auth.logout();
-
     this.router.navigate(['/login']);
   }
 }
